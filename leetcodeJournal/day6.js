ba83287c4cba692b7f643cc761a3ed203609e3a8
JC Trilamanila <trilamanila@gmail.com>
2022-01-07 21:00:49 -0500

    day 6

---
+++ leetcodeJournal/day6.js
@@ -0,0 +1,60 @@
+// 1/6/2022
+
+/*
+
+Single Number
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+
+*/
+
+var singleNumber = function(nums) {
+  if (nums.length === 1) {
+    return nums[0];
+  }
+
+  let result = 0;
+
+  nums.forEach(element => {
+    result = result ^ element;
+  });
+
+  return result;
+};
+
+/*
+Linked List Cycle
+
+Given head, the head of a linked list, determine if the linked list has a cycle in it.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Return true if there is a cycle in the linked list. Otherwise, return false.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where the tail connects to the 1st node (0-indexed).
+
+ */
+
+const hasCycle = function(head) {
+  let set = new Set();
+  while (head) {
+    if (set.has(head)) return true;
+    set.add(head);
+    head = head.next;
+  }
+  return false;
+};
