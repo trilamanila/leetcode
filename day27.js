7c2324e55316c2aa8e3d5539ed33e30a080b846f
JC Trilamanila <trilamanila@gmail.com>
2022-01-29 17:03:52 -0500

    day 28

--- day27.js
+++ day27.js
@@ -60,7 +60,6 @@ const validMountainArray = function(arr) {
 
 /*
 Replace Elements with Greatest Element on Right Side
-Replace Elements with Greatest Element on Right Side
 
 Given an array arr, replace every element in that array with the greatest element among the elements to its right, and replace the last element with -1.
 
