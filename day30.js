75acd7a5a2b159427bc5401e43cd05fa610ae176
Joncin Picol <jctrilamanila@gmail.com>
2022-02-01 20:24:57 -0500

    Day 31

--- day30.js
+++ day30.js
@@ -37,3 +37,71 @@ heights:  [1,2,3,4,5]
 expected: [1,2,3,4,5]
 All indices match.
 */
+
+var heightChecker = function(heights) {
+  let heightOrdered = heights.slice().sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== heightOrdered[i]) {
+      count++;
+    }
+  }
+  return count;
+};
+
+/*
+Third Maximum Number
+
+Given an integer array nums, return the third distinct maximum number in this array. If the third maximum does not exist, return the maximum number.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2.
+The third distinct maximum is 1.
+
+Example 2:
+
+Input: nums = [1,2]
+Output: 2
+Explanation:
+The first distinct maximum is 2.
+The second distinct maximum is 1.
+The third distinct maximum does not exist, so the maximum (2) is returned instead.
+
+Example 3:
+
+Input: nums = [2,2,3,1]
+Output: 1
+Explanation:
+The first distinct maximum is 3.
+The second distinct maximum is 2 (both 2's are counted together since they have the same value).
+The third distinct maximum is 1.
+*/
+
+var thirdMax = function(nums) {
+  let max = -Infinity,
+    mid = -Infinity,
+    min = Infinity;
+
+  for (let num of nums) {
+    if (num > max) {
+      min = mid;
+      mid = max;
+      max = num;
+    } else if (num > mid && num < max) {
+      min = mid;
+      mid = num;
+    } else if (num > min && num < mid) {
+      min = num;
+    }
+  }
+  return min > -Infinity ? min : max;
+};
+
+thirdMax([3, 2, 1]);
---
+++ day31.js
@@ -0,0 +1,29 @@
+//Day 31 1/31/2022
+/*
+Find All Numbers Disappeared in an Array
+
+Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+*/
+
+var findDisappearedNumbers = function(nums) {
+  let result = [];
+
+  for (let i = 1; i < nums.length + 1; i++) {
+    if (!nums.includes(i)) {
+      result.push(i);
+    }
+  }
+  return result;
+};
