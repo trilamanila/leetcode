6267cd5a0506098a2559b7c5b864b51462f0b32a
Joncin Picol <jctrilamanila@gmail.com>
2022-02-10 12:41:50 -0500

    Day 6

---
+++ leetcodeJournal/February/day6.js
@@ -0,0 +1,79 @@
+//Feb 6th 2/6/2022
+/*
+Array Partition I
+
+Given an integer array nums of 2n integers, group these integers into n pairs (a1, b1), (a2, b2), ..., (an, bn) such that the sum of min(ai, bi) for all i is maximized. Return the maximized sum.
+
+ 
+
+Example 1:
+
+Input: nums = [1,4,3,2]
+Output: 4
+Explanation: All possible pairings (ignoring the ordering of elements) are:
+1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+So the maximum possible sum is 4.
+
+Example 2:
+
+Input: nums = [6,2,6,5,1,2]
+Output: 9
+Explanation: The optimal pairing is (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9.
+*/
+
+const arrayPairSum = function (nums) {
+  nums.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < nums.length; i += 2) {
+    sum += Math.min(nums[i], nums[i + 1]);
+  }
+  return sum;
+};
+
+/*
+Two Sum II - Input array is sorted
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. You may not use the same element twice.
+
+Your solution must use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+*/
+
+var twoSum = function (numbers, target) {
+  let left = 0;
+  let right = numbers.length - 1;
+  while (left < right) {
+    const cur = numbers[left] + numbers[right];
+    //Return 1 base index if the current sum is equal to the target
+    if (cur === target) return [left + 1, right + 1];
+    // Because the array is sorted, anytime we move a pointer to the right numbers will get bigger and anytime we move a pointer to the left, numbers will get smaller
+    // If cur is greater than target that means we need to use smaller numbers in our sum and we move the right pointer to the left.
+    // If cur is less than target that means we need to use bigger number in our sum and we move the left pointer to the right.
+    cur > target ? right-- : left++;
+  }
+};
